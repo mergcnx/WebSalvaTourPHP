cb8b09998bfb473a48432db421b2b897eae8a9cf
FarneseG <joelgrandejoel882@gmail.com>
2023-04-14 16:51:18 -0600

    plantilla volcan

---
+++ pages/volcanchap.php
@@ -0,0 +1,196 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Destino - SalvaTour</title>
+    <link rel="stylesheet" href="../css/plvcr.css">
+</head>
+
+
+<body>
+<a href="inicio" id="inicio"></a>
+    <?php include '../layout/_menu-pages.php'; ?>
+
+    <section class="header-plantilla" style="background-image: url(../img/volcanchaparrastique/volcha_b.jpg);">
+        <div class="banner">
+            <h1>VOLCAN CHAPARRASTIQUE</h1>
+            <br>
+            <br>
+            <p>El Volcán de San Miguel - Chaparrastique es el 3er volcán más alto de El Salvador con 2.130 msnm abarcando 13 km cuadrados de terreno y un cráter de 800 m.</p>
+            <a href="../carrito.php" class="boton-pl">Reservar</a>
+        </div>
+    </section>
+    <!--=======================================================================================-->
+    <main class="contenedor-pri">
+        <!--========================================================================================-->
+        <section class="conteiner-A">
+            <div class="text">
+                <a href="bosimpo" id="bosimpo"></a>
+                <h2 class="title">Volcan Chaparrastique</h2>
+                <div class="intro">
+                    <div class="parr">
+                        <p class="text-intro">
+                        ¿Sabías que el Volcán Chaparrastique es el también llamado Volcán de San Miguel? Así es y además, su nombre es de origen lenca y quiere decir “tierra del calor”. Algo que no podría ser diferente pues se trata de uno de los volcanes más activos de El Salvador.<br>Sin embargo, aunque se caracteriza por su gran actividad volcánica, es uno de los destinos turísticos más importantes del país. Esto se debe a que es el tercero más grande y su vista desde la cima es increíble
+                        </p>
+                    </div>
+                    <div class="menu-interno">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--==========================================================================================================================================-->
+        <article class="articulos">
+            <a href="actividad" id="actividad"></a>
+            <h2 class="subtitle">Actividades que podemos realizar en el Volcan chaparrastique</h2>
+            <div class="cont-art">
+                <div class="cont-par">
+                    <div class="parr2">
+                        <div class="imge"><img src="../img/volcanchaparrastique/volcha_1.jpg" alt=""></div>
+                        <p class="text-info-pl">
+                        El Volcán de Chaparrastique es uno de los destinos preferidos de aquellos que aman la aventura y adoran escalar altas cumbres. Por lo que el ascenso es un reto para los más valientes, considerando que deben tener gran condición física. Por lo tanto, representa un ascenso difícil con un empinado de 45° en el que además, se podrá apreciar un cambio de temperatura importante. Sin embargo, para los que no poseen las condiciones que requieren esta aventura, pueden realizar un poco de senderismo en los alrededores.
+                        </p>
+                    </div>
+                    <div class="parr2">
+                        <div class="imge"><img src="../img/volcanchaparrastique/volcha_2.jpg" alt=""></div>
+                        <p class="text-info-pl">
+                        De este modo, podrás disfrutar también del paisaje, grandes vistas y conocer toda la biodiversidad del lugar. Sin importar el tipo de recorrido que deseas realizar, es importante que sepas que el mismo inicia en las plantaciones de café de Placitas. Por lo que es un camino bastante extenso hasta poder ver las laderas del volcán. El tiempo de recorrido varía, si quieres hacer el ascenso y descenso esto tomará todo el día.</p>
+                    </div>
+                    <div class="parr2">
+                        <div class="imge"><img src="../img/volcanchaparrastique/volcha_3.jpg" alt=""></div>
+                        <p class="text-info-pl">El Volcán Chaparrastique se encuentra a 11 km de la ciudad de San Miguel. Lo interesante es que constituye la única elevación en 100 m2 de una ladera que deja ver a sus espaldas, el Volcán Payacal, ya extinto. Por otra parte, se trata del 3er volcán más alto de El Salvador con 2.130 msnm abarcando 13 km2 de terreno y un cráter de 800 m aproximadamente. Adicionalmente, se encuentra dentro de las categorías de volcán activo dada a las cuantiosas erupciones que ha protagonizado en más de 300 años.</p>
+                    </div>
+                </div>
+                <div class="cont-mapa">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d73837.0589005412!2d-88.29810459476806!3d13.43815719218473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x8f7b2e97f481f6cd%3A0xffb8da74a2b270d2!2svolcan%20chaparrastique!3m2!1d13.4333333!2d-88.2666667!5e0!3m2!1ses!2ssv!4v1681500180965!5m2!1ses!2ssv" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </article>
+    </main>
+    <!--==========================================================================================================================================-->
+    <section class="conteiner-B">
+        <div class="title-r">
+            <a href="expe" id="expe"></a>
+            <h1>EXPERIENCIA EN Parque Nacional Montecristo</h1>
+        </div>
+        <!----------------------------------------------------------------------------------------------------------->
+        <div class="ayuda">
+            <div class="card-grid-pl">
+                <a class="card-plg" href="#">
+                <div class="card__background" style="background-image: url(../img/volcanchaparrastique/volcha_4.jpg)"></div>
+                <div class="card__content">
+                    <p class="card__category">Volcan chaparrastique</p>
+                    <h3 class="card__heading">Crater</h3>
+                </div>
+                </a>
+                <a class="card-plg" href="#">
+                <div class="card__background" style="background-image: url(../img/volcanchaparrastique/volcha_5.jpg)"></div>
+                <div class="card__content">
+                    <p class="card__category">Volcan chaparrastique</p>
+                    <h3 class="card__heading">Volcan</h3>
+                </div>
+                </a>
+                <a class="card-plg" href="#">
+                <div class="card__background" style="background-image: url(../img/volcanchaparrastique/volcha_6.jpg)"></div>
+                <div class="card__content">
+                    <p class="card__category">volcan chaparrastique</p>
+                    <h3 class="card__heading">Termina</h3>
+                </div>
+                </li>
+                <a class="card-plg" href="#">
+                <div class="card__background" style="background-image: url(../img/volcanchaparrastique/volcha_7.jpg)"></div>
+                <div class="card__content">
+                    <p class="card__category">volcan chaparrastique</p>
+                    <h3 class="card__heading">sdfsdfsdf</h3>
+                </div>
+                </a>
+            </div>
+        </div>
+        <!----------------------------------------------------------------------------------------------------------->
+
+    </section>
+    <!--===================================================================================================================-->
+
+    <!--==================================================== PRECIOS =======================================================-->
+    <main class="contenedor-pri">
+        <section class="precios">
+            <div class="title-pr">
+                <h1>Reserva para tener una mejor experiencia</h1>
+                <p>El clima en el Volcán Chaparrastique y San Miguel en general, es de tipo Sabana Caliente Tropical con temperaturas entre 30°C y 40°C, teniendo como referencia media al día 36°C.</p>
+            </div>
+            <div class="space"></div>
+            <div class="cont-pre">
+                <div class="menu-pl">
+                    <nav>
+                        <ul>
+                            <li><a href="#inicio">Inicio</a></li>
+                            <li><a href="#bosimpo">Volcan chaparrastique</a></li>
+                            <li><a href="#actividad">Actividades</a></li>
+                            <li><a href="#expe">Experiencia</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="btnR">
+                    <a href="../carrito.php" class="boton-pl boton">Reservar</a>
+                </div>
+            </div>
+            <div class="cont-pre-1">
+                <div class="col" ontouchstart="this.classList.toggle('hover');">
+                    <div class="container">
+                        <div class="front" style="background-image: url(../img/volcanchaparrastique/vctarjeta_1.jpg)">
+                            <div class="inner">
+                                <p>Entrada:</p>
+                            </div>
+                        </div>
+                        <div class="back">
+                            <div class="inner">
+                                <p>Salvadoreños: 5.00$ <br> Extranjeros: 10.00$</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col" ontouchstart="this.classList.toggle('hover');">
+                    <div class="container">
+                        <div class="front" style="background-image: url(../img/volcanchaparrastique/vctarjeta_2.jpg)">
+                            <div class="inner">
+                                <p>Horarios:</p>
+                            </div>
+                        </div>
+                        <div class="back">
+                            <div class="inner">
+                                <p>Lunes - Viernes <br> 7:00am - 3:00pm <br> Sabados - Domingo <br> 8:00am - 2:00pm</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col" ontouchstart="this.classList.toggle('hover');">
+                    <div class="container">
+                        <div class="front" style="background-image: url(../img/volcanchaparrastique/vctarjeta_3.jpg)">
+                            <div class="inner">
+                                <p>Parqueo:</p>
+                            </div>
+                        </div>
+                        <div class="back">
+                            <div class="inner">
+                                <p>Automoviles livianos 2.00$<br> Vehiculos pesados 4.00$</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!--====================================================================================================================-->
+
+    <?php include '../layout/_footer.php'; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+
+</body>
